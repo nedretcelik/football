2fe742703535bdd4fccef3aaaa1fdef16e115888
nedretcelik <nedretcelik97@gmail.com>
2024-05-09 14:36:39 -0400

    display football team name and code

--- scripts/index.js
+++ scripts/index.js
@@ -6,13 +6,18 @@ let teams = [
   { code: "KAN", name: "Kansas City Chiefs", plays: "Kansas City, MO" },
 ];
 
- function init() {
+function init() {
+  const displayTeamButton = document.getElementById("displayTeamButton");
+  displayTeamButton.onclick = displayTeam;
+  
 
-    selectTeamOption();
-
-};
+  selectTeamOption();
+}
 
 const footballTeamList = document.getElementById("footballTeamList");
+const showTeam = document.getElementById("showTeam");
+const valueOfTeam = document.getElementById("valueOfTeam");
+
 function selectTeamOption() {
   for (const team of teams) {
     const option = document.createElement("option");
@@ -24,8 +29,16 @@ function selectTeamOption() {
   }
 }
 
+function displayTeam() {
+  const selectCode = footballTeamList.value;
+  for(const team of teams) {
+    if(team.code == selectCode) {
+        showTeam.innerText = team.name;
+        valueOfTeam.innerText = team.code;
+    }
+  }
+
+}
+
+
 window.onload = init;
-
-
-
-
